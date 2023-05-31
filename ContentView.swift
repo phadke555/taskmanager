28326b135fb65cff42079b478abf9d715f1bb4ba
phadke555 <118079186+phadke555@users.noreply.github.com>
2023-05-31 14:58:46 -0400

    Add files via upload

--- ContentView.swift
+++ ContentView.swift
@@ -1,9 +1,10 @@
 //
 //  ContentView.swift
-//  taskmanager
+//  taskmanagerplus
 //
-//  Created by Rohan Phadke on 5/24/23.
+//  Created by Rohan Phadke on 5/31/23.
 //
+
 
 import SwiftUI
 
@@ -11,14 +12,20 @@ struct TodoItem: Identifiable, Equatable {
     let id = UUID()
     var task: String
     var isCompleted: Bool = false
+    var listName: String
 }
 
 class TodoListViewModel: ObservableObject {
     @Published var items: [TodoItem] = []
-    
-    func addItem(task: String) {
-        let newItem = TodoItem(task: task)
+    var taskLists: [String] = []
+
+    func addItem(task: String, listName: String) {
+        let newItem = TodoItem(task: task, listName: listName)
         items.append(newItem)
+        
+        if !taskLists.contains(listName) {
+            taskLists.append(listName)
+        }
     }
     
     func deleteItem(at index: Int) {
@@ -30,60 +37,96 @@ class TodoListViewModel: ObservableObject {
     }
 }
 
-struct TodoListView: View {
-    @ObservedObject var viewModel = TodoListViewModel()
+struct TaskListView: View {
+    @ObservedObject var viewModel: TodoListViewModel
     @State private var newTask: String = ""
+    let listName: String
+
+    var body: some View {
+        VStack {
+            List {
+                ForEach(viewModel.items.filter({ $0.listName == listName })) { item in
+                    Button(action: {
+                        viewModel.toggleCompletion(at: viewModel.items.firstIndex(of: item)!)
+                    }) {
+                        HStack {
+                            Image(systemName: item.isCompleted ? "checkmark.circle.fill" : "circle")
+                                .foregroundColor(item.isCompleted ? .green : .primary)
+                            Text(item.task)
+                                .strikethrough(item.isCompleted)
+                            Spacer()
+                                                        
+                            Button(action: {
+                                viewModel.deleteItem(at: viewModel.items.firstIndex(of: item)!)
+                            }) {
+                                Image(systemName: "xmark.circle.fill")
+                                    .foregroundColor(.red)
+                            }
+                        }
+                    }
+                }
+                .onDelete { indexSet in
+                    viewModel.deleteItem(at: indexSet.first!)
+                }
+            }
+            .listStyle(PlainListStyle())
+            
+            HStack {
+                TextField("Enter task", text: $newTask)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                Button(action: {
+                    viewModel.addItem(task: newTask, listName: listName)
+                    newTask = ""
+                }) {
+                    Text("Add")
+                }
+            }
+            .padding()
+        }
+        .navigationBarTitle(listName)
+    }
+}
+
+struct HomeView: View {
+    @ObservedObject var viewModel: TodoListViewModel
+    @State private var newListName: String = ""
     
     var body: some View {
         NavigationView {
             VStack {
-                List {
-                    ForEach(viewModel.items) { item in
-                        Button(action: {
-                            viewModel.toggleCompletion(at: viewModel.items.firstIndex(of: item)!)
-                        }) {
-                            HStack {
-                                Image(systemName: item.isCompleted ? "checkmark.circle.fill" : "circle")
-                                    .foregroundColor(item.isCompleted ? .green : .primary)
-                                Text(item.task)
-                                    .strikethrough(item.isCompleted)
-                                Spacer()
-                                                            
-                                Button(action: {
-                                            viewModel.deleteItem(at: viewModel.items.firstIndex(of: item)!)
-                                                            }) {
-                                                                Image(systemName: "xmark.circle.fill")
-                                                                    .foregroundColor(.red)
-                                                            }
-                            }
-                        }
-                    }
-                    .onDelete { indexSet in
-                        viewModel.deleteItem(at: indexSet.first!)
+                List(viewModel.taskLists, id: \.self) { listName in
+                    NavigationLink(destination: TaskListView(viewModel: viewModel, listName: listName)) {
+                        Text(listName)
                     }
                 }
-                .listStyle(PlainListStyle())
                 
                 HStack {
-                    TextField("Enter task", text: $newTask)
+                    TextField("Enter new list name", text: $newListName)
                         .textFieldStyle(RoundedBorderTextFieldStyle())
                     Button(action: {
-                        viewModel.addItem(task: newTask)
-                        newTask = ""
+                        viewModel.taskLists.append(newListName)
+                        newListName = ""
                     }) {
-                        Text("Add")
+                        Text("Create List")
                     }
                 }
                 .padding()
             }
-            .background(Color.init(red: 0.2, green: 0.5, blue: 0.5, opacity: 0.5))
+            .navigationBarTitle("Task Lists")
         }
     }
 }
 
-struct TodoListView_Previews: PreviewProvider {
-    static var previews: some View {
-        TodoListView()
+struct ContentView: View {
+    @StateObject var viewModel = TodoListViewModel()
+    
+    var body: some View {
+        HomeView(viewModel: viewModel)
     }
 }
 
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ taskmanagerplusApp.swift
@@ -0,0 +1,17 @@
+//
+//  taskmanagerplusApp.swift
+//  taskmanagerplus
+//
+//  Created by Rohan Phadke on 5/31/23.
+//
+
+import SwiftUI
+
+@main
+struct taskmanagerplusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
